df160f9436da2b152ca4c273ff0fcae6f0a7102a
taufik69 <taufik35-2915@diu.edu.bd>
2025-11-03 19:24:12 +0600

    add routes for editing categories and subcategories

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { BannerList } from "./components/dashboard/banner/BannerList";
 import { CreateCategory } from "./components/dashboard/category/CreateCategory";
 import { CategoryList } from "./components/dashboard/category/categoryList";
 import { EditBanner } from "./components/dashboard/banner/EditBanner";
+import { EditCategory } from "./components/dashboard/category/CategoryEdit";
+import { CreateSubcategory } from "./components/dashboard/subcategory/CreateSubcategory";
+import { SubCategoryList } from "./components/dashboard/subcategory/SubcategoryList";
+import { EditSubCategory } from "./components/dashboard/subcategory/EditSubcategory";
 
 const App = () => {
   return (
@@ -16,6 +20,10 @@ const App = () => {
           <Route path="editBanner/:id" element={<EditBanner />} />
           <Route path="createCategory" element={<CreateCategory />} />
           <Route path="categorylist" element={<CategoryList />} />
+          <Route path="editcategory/:id" element={<EditCategory />} />
+          <Route path="createSubcategory" element={<CreateSubcategory />} />
+          <Route path="subcategorylist" element={<SubCategoryList />} />
+          <Route path="editsubcategory/:id" element={<EditSubCategory />} />
         </Route>
       </Routes>
     </BrowserRouter>
